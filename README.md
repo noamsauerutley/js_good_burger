5838d5bd7c2805a7c48ee69c893969c41d0b43f5
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-11-01 13:55:07 -0400

    Done.

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-## Welcome to Good Burger, the home of the Good Burger.
+<!-- ## Welcome to Good Burger, the home of the Good Burger.
 Welcome to your first day of work at Good Burger, the home of the Good Burger.
 As an engineer, you are tasked to design a web application in which customers can see our menu, order burgers as well as make custom orders.
 
-Right now, our burgers only exist in the database and we need your help to append it to the menu.
+Right now, our burgers only exist in the database and we need your help to append it to the menu. 
 Here's how we can run our database:
 ```
 TO INSTALL: npm install -g json-server
 TO START: json-server --watch db.json
-```
+``` -->
 
-Each burger that we `fetch` from the database should have the following classes when appending it to the DOM:
+<!-- Each burger that we `fetch` from the database should have the following classes when appending it to the DOM:
 
 ```
 <div class="burger">
@@ -20,11 +20,11 @@ Each burger that we `fetch` from the database should have the following classes 
     </p>
     <button class="button">Add to Order</button>
 </div>
-```
+``` -->
+<!-- 
+After the burgers are appended to the `burger-menu`, customers can press on the `Add to Order` button, which will then append the name of the burger into the 'Your Order' list on the left of the page. For now, do not worry about the repeats. If a customer orders two Good Burgers, you can (and should) have two Good Burgers on your order list. -->
 
-After the burgers are appended to the `burger-menu`, customers can press on the `Add to Order` button, which will then append the name of the burger into the 'Your Order' list on the left of the page. For now, do not worry about the repeats. If a customer orders two Good Burgers, you can (and should) have two Good Burgers on your order list.
-
-After that, build out the functionality that will allow customers to add their own burger creations to the menu. After submitting the form, the burger should be appended to the menu with the correct information typed in the form. It should also be added to customer's order as well as persist in the database, so that when we refresh the page, the burger is added to the menu for future customers.
+<!-- After that, build out the functionality that will allow customers to add their own burger creations to the menu. After submitting the form, the burger should be appended to the menu with the correct information typed in the form. It should also be added to customer's order as well as persist in the database, so that when we refresh the page, the burger is added to the menu for future customers. -->
 
 ## Demonstration
 ![](Demonstration.gif)
--- index.js
+++ index.js
@@ -1,3 +1,101 @@
 document.addEventListener("DOMContentLoaded", () => {
   //Implement Your Code Here
+  const menu = document.querySelector("#burger-menu")
+  const orderQueue = document.querySelector("#order-list")
+  const customOrder = document.querySelector("#custom-burger")
+
+  function createBurger(burger){
+      // create burger div
+      let burgerDiv = document.createElement("div")
+      burgerDiv.setAttribute("class", "burger")
+      burgerDiv.setAttribute("id", burger.id)
+
+      // create title
+      let burgerTitle = document.createElement("h3")
+      burgerTitle.setAttribute("class", "burger_title")
+      burgerTitle.innerText = burger.name
+      // append to burger
+      burgerDiv.appendChild(burgerTitle)
+
+      // create image
+      let burgerImg = document.createElement("img")
+      burgerImg.src = burger.image
+      // append to burger
+      burgerDiv.appendChild(burgerImg)
+
+      // create description
+      let burgerDesc = document.createElement("p")
+      burgerDesc.setAttribute("class", "burger_description")
+      burgerDesc.innerText = burger.description
+      // append to burger
+      burgerDiv.appendChild(burgerDesc)
+
+      // create button
+      let burgerButton = document.createElement("button")
+      burgerButton.setAttribute("class", "button")
+      burgerButton.innerText = "Add to Order"
+      // append to burger
+      burgerDiv.appendChild(burgerButton)
+      
+      // add button click event handler
+      burgerButton.addEventListener("click", (event)=>{
+        // create new order
+        let newOrder = document.createElement("li")
+        newOrder.innerText = burger.name
+        // append new order to queue
+        orderQueue.appendChild(newOrder)
+      })
+
+      //append burger to menu
+      menu.appendChild(burgerDiv)
+  }
+
+  // first, get all burgers:
+  fetch('http://localhost:3000/burgers')
+  .then(response => response.json())
+  .then(parsedFetch=>{
+    parsedFetch.forEach(burger => {
+      createBurger(burger)
+    })
+
+    customOrder.addEventListener("submit", (event) => {
+      // prevent default behavior so we can grab those form values
+      event.preventDefault()
+      
+      // grab those form values
+      let nameValue = event.target.name.value
+      let descValue = event.target.description.value
+      let imgValue = event.target.url.value
+
+      // success check
+      console.log("form submitted!")
+
+      fetch('http://localhost:3000/burgers', {
+        method: 'POST',
+        headers: {
+          "Content-Type" : "application/json",
+          "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+          // post form values to db
+          name: nameValue,
+          description: descValue,
+          image: imgValue
+        })
+      })
+        // json parse the data
+        .then(response => response.json())
+        .then(parsedData =>{
+          // bonus: append burger w/o refreshing
+          createBurger(parsedData)
+          // success check
+          console.log(parsedData)
+        })
+      })
+  })
+
+
+
+
+
 })
